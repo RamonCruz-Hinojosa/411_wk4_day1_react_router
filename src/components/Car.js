72b52b56a020caf522f8dbe56b24b9af044dd05d
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2021-12-08 14:23:08 -0600

    adjust container and paper to fit in the example

--- src/components/Car.js
+++ src/components/Car.js
@@ -8,20 +8,26 @@ const Car = (props) => {
   let id = props.match.params.id - 1;
   console.log(cars[id].Name);
   return (
-    <Container maxWidth="sm">
-      <Paper>
-        <Chip label={cars[id].id} />
-        <Chip label={cars[id].Name} />
-        <Chip label={cars[id].Miles_per_Gallon} />
-        <Chip label={cars[id].Cylinders} />
-        <Chip label={cars[id].Displacement} />
-        <Chip label={cars[id].Horsepower} />
-        <Chip label={cars[id].Weight_in_lbs} />
-        <Chip label={cars[id].Acceleration} />
-        <Chip label={cars[id].Year} />
-        <Chip label={cars[id].Origin} />
-      </Paper>
-    </Container>
+    <div className="car-container">
+      <Container>
+        <Paper
+          sx={{
+            width: 1 / 2,
+          }}
+        >
+          <Chip label={cars[id].id} />
+          <Chip label={cars[id].Name} />
+          <Chip label={cars[id].Miles_per_Gallon} />
+          <Chip label={cars[id].Cylinders} />
+          <Chip label={cars[id].Displacement} />
+          <Chip label={cars[id].Horsepower} />
+          <Chip label={cars[id].Weight_in_lbs} />
+          <Chip label={cars[id].Acceleration} />
+          <Chip label={cars[id].Year} />
+          <Chip label={cars[id].Origin} />
+        </Paper>
+      </Container>
+    </div>
   );
 };
 
